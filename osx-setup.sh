a701c150c2df20e86d3165ca79aa11a599a87a41
Sinan Bolel <sbolel@users.noreply.github.com>
2017-02-02 15:27:55 -0600

    Update osx-setup.sh

--- osx-setup.sh
+++ osx-setup.sh
@@ -42,17 +42,16 @@ tap_brew(){
 
 install_apps(){
   # apps
-  brew cask install 1password dropbox google-chrome google-drive  # core apps
-  brew cask install polymail slack spotify skype                  # communication
-  brew cask install logitech-unifying logitech-control-center     # logitech
-  # git, editors
-  brew install git tig bash-completion                         # git
-  brew install vim emacs && brew linkapps emacs                # sys editors
-  brew cask install iterm2-beta atom sublime-text webstorm     # app editors
-  # dev 
-  brew install chromedriver docker mysql ruby s3cmd wget yarn  # core dev
-  brew cask install virtualbox virtualbox-extension-pack       # vm apps
-  brew cask install google-cloud-sdk
+  brew cask install 1password dropbox spotify
+  brew cask install google-chrome google-drive
+  brew cask install polymail slack skype
+  brew cask install logitech-control-center logitech-unifying
+  # dev
+  brew install git tig bash-completion    # git
+  brew install vim emacs && brew linkapps emacs
+  brew install docker google-cloud-sdk mongodb mongoose mysql ruby s3cmd wget yarn
+  brew cask install iterm2-beta atom sublime-text webstorm # editors
+  brew cask install virtualbox virtualbox-extension-pack   # vm
 }
 
 install_nvm(){
@@ -63,20 +62,26 @@ install_nvm(){
 }
 
 install_node(){
-  nvm install v7.3.0
-  nvm alias default v7.3.0
+  nvm install v6.9.5
+  nvm alias default v6.9.5
   nvm use default
 }
 
 install_npm_globals(){
+  npm install -g electron cordova node-sass
+  npm install -g aurelia-cli babel-cli github:gulpjs/gulp#4.0
+}
+
+update_npm_all(){
   npm install -g npm
-  npm install -g node-pre-gyp node-gyp node-sass
   npm install -g npm-check npm-check-updates npm-run-all
-  npm install -g node-inspector nodemon supervisor watchman webdriver-manager
-  npm install -g typescript@>=2.1.4 typings webpack@>=2.2.0-rc.3 webpack-dev-server@>=2.2.0-rc.0
-  npm install -g babel-cli angular-cli aurelia-cli gulp-cli firebase-tools 
-  npm install -g eslint standard tslint karma karma-cli karma-webpack protractor phantomjs
-  npm install -g electron cordova cordova-browser ios-deploy ios-sim ionic
+  npm install -g typescript@next typings tslint
+  npm install -g cordova electron node-sass aurelia-cli github:gulpjs/gulp#4.0
+  npm install -g node-inspector nodemon supervisor watchman
+  npm install -g angular-cli aurelia-cli babel-cli firebase-tools uglifyjs github:gulpjs/gulp#4.0
+  npm install -g webpack@>=2.2.0-rc.3 webpack-dev-server@>=2.2.0-rc.0
+  npm install -g cordova cordova-browser ios-deploy ios-sim ionic
+  npm install -g eslint karma-cli phantomjs protractor standard webdriver-manager
 }
 
 # optional
