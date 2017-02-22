96b3075a45550617fa228e1d73025f1027b8589a
Sinan Bolel <sbolel@users.noreply.github.com>
2017-02-22 12:16:14 -0600

    Update osx-setup.sh

--- osx-setup.sh
+++ osx-setup.sh
@@ -11,17 +11,17 @@ install_brew
 install_apps
 install_nvm
 install_node
-install_npm_globals
 refresh_brew
 symlink_jsc
 config_git
+speedup_osx
 
 #############
 # functions #
 #############
 
 install_brew(){
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  cd && mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
   tap_brew      # update taps
   refresh_brew  # update, cleanup
 }
@@ -32,7 +32,7 @@ refresh_brew(){
 }
 
 tap_brew(){
-  brew tap homebrew/dupes 
+#   brew tap homebrew/dupes 
   brew tap homebrew/services 
   brew tap homebrew/versions 
   brew tap homebrew/php 
@@ -45,8 +45,8 @@ install_apps(){
   brew install wget gpg2 git tig bash-completion
   brew install vim emacs && brew linkapps emacs
   brew install docker google-cloud-sdk mongodb mongoose mysql ruby s3cmd yarn
-  brew cask install iterm2-beta atom sublime-text webstorm # editors
-  brew cask install virtualbox virtualbox-extension-pack   # vm
+  brew cask install iterm2-beta sublime-text
+  brew cask install virtualbox virtualbox-extension-pack
   # apps
   brew cask install 1password dropbox spotify
   brew cask install google-chrome google-drive
@@ -65,23 +65,6 @@ install_node(){
   nvm install 6.10.0
   nvm alias default 6.10.0
   nvm use default
-}
-
-install_npm_globals(){
-  npm install -g electron cordova node-sass
-  npm install -g aurelia-cli babel-cli github:gulpjs/gulp#4.0
-}
-
-update_npm_all(){
-  npm install -g npm
-  npm install -g npm-check npm-check-updates npm-run-all
-  npm install -g typescript@next typings tslint
-  npm install -g cordova electron node-sass aurelia-cli github:gulpjs/gulp#4.0
-  npm install -g node-inspector nodemon supervisor watchman
-  npm install -g angular-cli aurelia-cli babel-cli firebase-tools uglifyjs github:gulpjs/gulp#4.0
-  npm install -g webpack@>=2.2.0-rc.3 webpack-dev-server@>=2.2.0-rc.0
-  npm install -g cordova cordova-browser ios-deploy ios-sim ionic
-  npm install -g eslint karma-cli phantomjs protractor standard webdriver-manager
 }
 
 # optional
@@ -112,7 +95,7 @@ config_git(){
   read email
   git config --global user.name "${name}"
   git config --global user.email "${email}"
-  git config --global core.editor emacs
+  git config --global core.editor nano
   git config --global push.default current
 }
 
